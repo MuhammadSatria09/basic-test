abe1505c60dbd1ebf5feb2aac6e67cb97ecc67c7
MuhammadSatria <92299364+MuhammadSatria09@users.noreply.github.com>
2021-12-18 07:36:40 +0700

    Add files via upload

---
+++ 2157051010_Muhammad Satria_Postest4.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+ cout<<"------------------------------------------------------------------------"<<endl;
+	cout<<"Muhammad Satria aulia\n2157051010\n\n";
+	cout<<"------------------------------------------------------------------------"<<endl;
+	
+	int a;
+	
+	cout<<"Angka =";cin>>a;
+	
+	switch(a){
+		case 1:
+			cout<<"satu";
+			break;
+		case 2:
+			cout<<"dua";
+			break;
+		case 3:
+			cout<<"tiga";
+			break;
+		case 4:
+			cout<<"empat";
+			break;
+		case 5:
+			cout<<"lima";
+			break;
+		case 6:
+			cout<<"enam";
+			break;
+		case 7:
+			cout<<"tujuh";
+			break;
+		case 8:
+			cout<<"delapan";
+			break;
+		case 9:
+			cout<<"sembilan";
+			break;
+		default:
+			cout<<a;
+	}
+}
